521ba4d43a748d3440a622509498fa3d5eb0fe13
Yashgiri <yashgoswami3251@gmail.com>
2026-08-25 08:33:42 +0530

    Program 17 in C sharp language

--- Program.cs
+++ Program.cs
@@ -70,7 +70,8 @@ namespace C_sharp_Language
             //t3_13.product();
             //t3_14.employee();
             //t3_15.inheritance();
-            t3_16.employee();
+            //t3_16.employee();
+            t3_17.method();
 
         }
     }
---
+++ Tutorial3/t3_17.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_Language.Tutorial3
+{
+    internal class t3_17
+    {
+        // Base class
+
+        class shape
+        {
+            private double area;
+
+            public virtual void calculateArea()
+            {
+                Console.WriteLine("Calculating area of shape");
+            }
+
+        }
+
+        class circle : shape
+        {
+            public override void calculateArea()
+            {
+                Console.WriteLine("Enter Radius of Circle");
+                double radius = Convert.ToDouble(Console.ReadLine());
+
+                double area = Math.PI * radius * radius;
+                Console.WriteLine("Area of Circle is : " + area);
+            }
+        }
+
+        class rectangle : shape
+        {
+            public override void calculateArea()
+            {
+                Console.WriteLine("Enter Length of Rectangle");
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter Width of Rectangle");
+                double width = Convert.ToDouble(Console.ReadLine());
+
+                double area = length * width;
+                Console.WriteLine("Area of Rectangle is : " + area);
+            }
+
+        }
+
+        class triangle : shape
+        {
+            public void calculateArea()
+            {
+                Console.WriteLine("Enter Base of Triangle");
+                double baseLength = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter Height of Triangle");
+                double baseHeight = Convert.ToDouble(Console.ReadLine());
+
+                double area = 0.5 * baseLength * baseHeight;
+                Console.WriteLine("Area of Triangle is : " + area);
+            }
+        }
+
+        public static void method()
+        {
+
+            circle c = new circle();
+            c.calculateArea();
+
+            rectangle r = new rectangle();
+            r.calculateArea();
+
+            triangle t = new triangle();
+            t.calculateArea();
+
+        }
+    }
+}
